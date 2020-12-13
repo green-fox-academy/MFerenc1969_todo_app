606332919b17eddbdee542dde6bd7190ee012efa
Mihalovits Ferenc <ferigithub69@gmail.com>
2020-12-13 17:42:17 +0100

    error handling

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+import minimist from "minimist";
 
-function render () {
-    console.log ("Parancssori Todo applikáció");
-    console.log ("=============================");
-    console.log ("Parancssori argumentumok:");
-    console.log ("-l   Kilistázza a feladatokat");
-    console.log ("-a   Új feladatot ad hozzá");
-    console.log ("-r   Eltávolít egy feladatot");
-    console.log ("-c   Teljesít egy feladatot");
+start();
+
+function render() {
+  console.log("Parancssori Todo applikáció");
+  console.log("=============================");
+  console.log("Parancssori argumentumok:");
+  console.log("-l   Kilistázza a feladatokat");
+  console.log("-a   Új feladatot ad hozzá");
+  console.log("-r   Eltávolít egy feladatot");
+  console.log("-c   Teljesít egy feladatot");
+}
+function start() {
+  const args = minimist(process.argv);
+  const argType = Object.keys(args).every((arg) => typeof arg === "string");
+  const valid = Object.keys(args).every((arg) =>
+    ["_", "l", "a", "r", "c"].includes(arg)
+  );
+  const must = args._.length === 2;
+  console.log(args);
+  if (argType === true && valid === true && must === true) {
+    console.log("helyes");
+  } else {
+    console.log("Nem támogatott agrumentum!");
+    render();
+  }
 }
