12b626a072b3650ded0a0c31e06052d785180e5b
Mihalovits Ferenc <ferigithub69@gmail.com>
2020-12-13 21:33:44 +0100

    delete + error handling

--- deletetodo.js
+++ deletetodo.js
@@ -1,3 +1,17 @@
-export function deleteTodo(params) {
-    console.log("törlés");
+import { todos } from "./classtodo.js";
+import { save } from "./save.js";
+
+export function deleteTodo(index) {
+  if (typeof index === "boolean") {
+    console.log("Nem lehetséges az eltávolítás : nem adtál meg indexet!");
+  } else if (typeof index !== "number") {
+    console.log("Nem lehetséges az eltávolítás : a megadott index nem szám");
+  } else if (index - 1 >= todos.length) {
+    console.log(
+      "Nem lehetséges az eltávolítás : túlindexelési probléma adódott!"
+    );
+  } else {
+    todos.splice(index - 1, 1);
+    save();
   }
+}
