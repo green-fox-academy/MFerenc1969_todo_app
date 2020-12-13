a465d83112ae4d4c2be2db8bfe4f3395f581e5f4
Mihalovits Ferenc <ferigithub69@gmail.com>
2020-12-13 17:09:41 +0100

    render

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MFerenc1969_todo_app
+npm init -y
+package.json kiegészítése a "type": "modoles" -lal
+npm install minimist
---
+++ index.js
@@ -0,0 +1,10 @@
+
+function render () {
+    console.log ("Parancssori Todo applikáció");
+    console.log ("=============================");
+    console.log ("Parancssori argumentumok:");
+    console.log ("-l   Kilistázza a feladatokat");
+    console.log ("-a   Új feladatot ad hozzá");
+    console.log ("-r   Eltávolít egy feladatot");
+    console.log ("-c   Teljesít egy feladatot");
+}
